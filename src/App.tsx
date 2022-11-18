8c246f2ca1f72cc86e79b40bfa07265d9ce3bf0d
jungminyoo <yoojm4718@outlook.com>
2022-11-18 14:36:53 +0900

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import { IShoppingSale } from "./api/structures/shoppings/sales/IShoppingSale";
+
+interface Props {
+  sale: IShoppingSale;
+}
+
+function App({ sale }: Props) {
+  return <div></div>;
+}
+
+export default App;
